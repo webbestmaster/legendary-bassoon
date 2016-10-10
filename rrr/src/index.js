6668f72ae202e6f57c446051104583e722bcb0bd
dim <web.best.master@gmail.com>
2016-10-10 23:22:58 +0300

    rrr - in progress...

--- rrr/src/index.js
+++ rrr/src/index.js
@@ -1,12 +1,18 @@
-import 'babel-polyfill';
-import React from 'react';
-import {render} from 'react-dom';
-import App from './containers/App';
+import    'babel-polyfill'
+import    React    from    'react'
+import    {render}    from    'react-dom'
+import    App    from    './containers/App'
+import    Admin    from    './components/Admin'
+import    Genre    from    './components/Genre'
+import    Home    from    './components/Home'
+import    {Router, Route, IndexRoute, browserHistory}    from    'react-router'
 render(
-    <App />,
+    <Router history={browserHistory}>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home}/>
+            <Route path='admin' component={Admin}/>
+            <Route path='genre' component={Genre}/>
+        </Route>
+    </Router>,
     document.getElementById('root')
 );
-
-
-
-
