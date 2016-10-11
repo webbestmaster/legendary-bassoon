5689cec4f5ec8ce4e017ecf4e6a991d82ad7e6bb
dmitriy.turovtsov <web.best.master@gmail.com>
2016-10-11 13:31:16 +0300

    redux - in progress...

--- rrr/src/index.js
+++ rrr/src/index.js
@@ -1,11 +1,12 @@
-import    'babel-polyfill'
-import    React    from    'react'
-import    {render}    from    'react-dom'
-import    App    from    './containers/App'
-import    Admin    from    './components/Admin'
-import    Genre    from    './components/Genre'
-import    Home    from    './components/Home'
-import    {Router, Route, IndexRoute, browserHistory}    from    'react-router'
+import    'babel-polyfill';
+import    React    from    'react';
+import    {render}    from    'react-dom';
+import    App    from    './containers/App';
+import    Admin    from    './components/Admin';
+import    Genre    from    './components/Genre';
+import    Home    from    './components/Home';
+import NotFound from './components/NotFound';
+import    {Router, Route, IndexRoute, browserHistory}    from    'react-router';
 render(
     <Router history={browserHistory}>
         <Route path='/' component={App}>
@@ -13,6 +14,8 @@ render(
             <Route path='admin' component={Admin}/>
             <Route path='genre' component={Genre}/>
         </Route>
+        {/* для всех остальных роутов: показывай NotFound */}
+        <Route path='*' component={NotFound} />
     </Router>,
     document.getElementById('root')
 );
